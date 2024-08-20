2e79e80ad7a8092f62278f035257eb979e17c654
hserhiis <serhiiswr1994@gmail.com>
2024-08-20 05:49:09 +0200

    Basic project

---
+++ QuietSpotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuietSpotsApp.swift
+//  QuietSpots
+//
+//  Created by Yevheniia Kolkova on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct QuietSpotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
